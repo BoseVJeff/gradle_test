bb978c722a32a3517b129b4282992e8c0baaf7ae
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-04-16 09:48:04 +0530

    Added basic UI with JavaFX

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,9 +5,25 @@ package org.example;
 
 import java.sql.*;
 
-public class App {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
     public String getGreeting() {
         return "Hello World!";
+    }
+
+    @Override
+    public void start(Stage stage) {
+        String javaVersion = System.getProperty("java.version");
+        String javafxVersion = System.getProperty("javafx.version");
+        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) throws SQLException {
@@ -18,5 +34,7 @@ public class App {
         databaseInterface.cleanup();
 
         System.out.println(new App().getGreeting());
+
+        launch();
     }
 }
